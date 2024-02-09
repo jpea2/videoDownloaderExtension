2a4470c79787b5a26fc24f98ca5db8f422abfcf1
Jay <124354310+jpea2@users.noreply.github.com>
2024-02-10 09:27:19 +1000

    initial files including extension and py script to download

---
+++ app.js
@@ -0,0 +1,75 @@
+const inputBtn = document.querySelector("#input-btn")
+let myVideos = []
+const clearInputBtn = document.querySelector("#clear-input-btn")
+const ulEl = document.querySelector("#ul-el")
+const inputEl = document.querySelector("#input-el")
+const deleteBtn = document.querySelector("#delete-btn")
+const inputTabBtn = document.querySelector("#input-tab-btn")
+const storedVideosLocal = JSON.parse(localStorage.getItem("myVideos"))
+const autoAddTab = document.querySelector("#auto-add-tab-btn")
+
+if (storedVideosLocal) {
+    myVideos = storedVideosLocal
+    render(myVideos)
+}
+
+clearInputBtn.addEventListener("click", function() {
+    if (inputEl.value) {
+        inputEl.value = ""
+    }
+})
+
+
+//.innerHTMl comes at a cost. So do we want it done once or for     
+//each time in the loop? Thats why we can set it once at the end of the loop
+
+autoAddTab.addEventListener("click", function(){
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myVideos.push(tabs[0].url)
+        localStorage.setItem("myVideos", JSON.stringify(myVideos))
+        render(myVideos)
+        updateListCount()
+    })
+})
+
+inputTabBtn.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    inputEl.value = tabs[0].url
+    })
+})
+
+function render(videos) {
+    let listItems = ""
+    // start // finish // increment
+    for (i = 0; i < videos.length; i++){
+        listItems += `
+        <li>
+            <a href='${videos[i]}' target='_blank'>
+                ${videos[i]} 
+            </a>
+        </li>` 
+    }
+    ulEl.innerHTML = listItems
+}
+
+//deletes local storage of saved links
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myVideos = []
+    render(myVideos) 
+})
+
+
+inputBtn.addEventListener("click", function () {
+    if (inputEl.value){
+        myVideos.push(inputEl.value)
+        render(myVideos)
+        localStorage.setItem("myVideos", JSON.stringify(myVideos))
+        console.log(localStorage.getItem("myVideos"))
+        inputEl.value = ""
+    }
+})
+
+render(myVideos)
+
+console.log(storedVideosLocal)
---
+++ download.py
@@ -0,0 +1,16 @@
+import os
+from pytube import YouTube
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        download_path = os.path.expanduser('~/Downloads')
+        youtubeObject.download(download_path)
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+
+link = input("Enter the YouTube video URL: ")
+Download(link)
